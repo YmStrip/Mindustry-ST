96022671914fa330180e37a4a98b823d17247a63
YmStrip <60353148+YmStrip@users.noreply.github.com>
2024-12-06 15:20:16 +0800

    -

--- src-ui/page/page/PageHome.ts
+++ src-ui/page/page/PageHome.ts
@@ -1,11 +1,36 @@
 import {Page} from "@/page/entity/Page.ts";
 // @ts-ignore
 import PageHomeComp from "@/page/page/PageHomeComp.vue";
+import {fs, Require} from "fyurry";
+
+const ch = Require("child_process");
 
 export class PageHome extends Page {
     component = PageHomeComp
+    cwd = process.cwd()
+
     constructor() {
         super("home");
         this.Icon("mdi:home")
+    }
+
+    run() {
+        //copy file
+        fs.copy(
+            this.cwd + '/build/libs/stDesktop.jar',
+            this.cwd + '/test/data/Mindustry/mods/stDesktop.jar'
+        )
+        const q = ch.exec(`start.bat`, {
+            cwd: `${this.cwd + "/test"}`
+        })
+        q.stdout.on('data', (d) => {
+            console.log(d)
+        })
+        q.stdout.on('error',d=>{
+            console.log(d)
+        })
+        q.stderr.on('data',d=>{
+            console.log(d)
+        })
     }
 }
--- src-ui/page/page/PageHomeComp.vue
+++ src-ui/page/page/PageHomeComp.vue
@@ -1,11 +1,27 @@
 <template>
   <LayoutFlex column width="100%" justify-content="flex-start" style="padding: 5px">
     <div style="width: 100%">
-      Side 1
+      <LayoutFlex>
+        <Button>
+          <Icon icon="mdi:cog"></Icon>
+        </Button><Input v-model:value="page.cwd" style="width: 100%"></Input>
+      </LayoutFlex>
+      <br>
+      <Button @click="page.run()" display="bracket">
+        <IconShadow icon="mdi:play"/>
+        PLAYGROUND
+      </Button>
     </div>
   </LayoutFlex>
 </template>
 <script lang="ts" setup>
 
 import LayoutFlex from "@/components/layout/LayoutFlex.vue";
+import Button from "@/components/button/Button.vue";
+import IconShadow from "@/module/icon/component/IconShadow.vue";
+import {PageProps} from "@/page/entity/PageProps.ts";
+import Input from "@/components/input/Input.vue";
+import {PageHome} from "@/page/page/PageHome.ts";
+import Icon from "@/module/icon/component/Icon.vue";
+const props= defineProps<PageProps<PageHome>>()
 </script>
